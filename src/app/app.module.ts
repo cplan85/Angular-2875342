844e91fe07c9b3e4cbf00bfc211a82b24235083b
Carlos Planchart <carlos_planchart@live.com>
2022-06-28 12:07:54 +0200

    created custom error handler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
+
+import { ErrorMetadataService } from './services/error-metadata.service';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { UserListComponent } from './user-list/user-list.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  //provide: is name of the class we want to override, useClass is class in place of Provide
+  providers: [{provide: ErrorHandler, useClass: ErrorMetadataService}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/error-metadata.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, ErrorHandler} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorMetadataService implements ErrorHandler {
+
+  public handleError(error: any): void {
+    const date = new Date();
+    console.error({
+      timeStamp: date.toISOString(),
+      message: error.message,
+      zone: error.zone
+    })
+  }
+
+}
