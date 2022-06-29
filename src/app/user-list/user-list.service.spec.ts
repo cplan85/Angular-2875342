ea18164c49261efb50381d491d08ec429a8b503f
Carlos Planchart <carlos_planchart@live.com>
2022-06-29 12:03:27 +0200

    fixed errors associated with userlist component spec

--- src/app/user-list/user-list.service.spec.ts
+++ src/app/user-list/user-list.service.spec.ts
@@ -1,16 +1,20 @@
+import { WebStorageService } from './../services/web-storage.service';
 import { TestBed } from '@angular/core/testing';
 
 import { UserListService } from './user-list.service';
 
-describe('UserListService', () => {
+describe('User List Service', () => {
   let service: UserListService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserListService);
+    service = new UserListService();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should return a User List with 16 users', (done: DoneFn) => {
+   service.getAll().then((response)=>{
+    expect(response.length).toBe(16);
+    done();
+   })
+  },// this sets timeout to 2 seconds
+   2000);
 });
